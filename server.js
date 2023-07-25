e1a0d279ba1faadd33ac83aec9c6f4eefb20c49f
zemtard <zemzard@gmail.com>
2023-07-25 11:58:37 +0200

    Added cors

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const cors = require('cors')
 
-//const server = require("http").createServer(app);
+app.use(cors({
+    origin: "http://127.0.0.1:5173",
+}))
+
+app.use(bodyParser.json());
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const users = {
+    'user1': { id: 'user1', password: 'zzz', name: 'User One' },
+    'user2': { id: 'user2', password: '123', name: 'User Two' },
+  };
 
 
 app.listen(port, hostname, () => {
@@ -21,7 +32,7 @@ app.get("/status", async (req, res) => {
 app.post('/user-login', (req, res) => {
 
     const requestData = req.body; //receive the request
-    console.log(req.body);
+    console.log(requestData);
     
     //TODO ENSURE USER EXISTS, IF EXISTS LOG HIM IN IF NOT DENY
 
@@ -32,6 +43,7 @@ app.post('/user-login', (req, res) => {
 app.post('/user-register', (req, res) => {
     
     const requestData = req.body; //receive the register request
+    console.log(requestData);
     
     //TODO ensure all fields are as expected, if they are - create new user
 
