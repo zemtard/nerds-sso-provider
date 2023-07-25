eb77ef0bd1268489b2bb2db7d765b9fc2236afd5
zemtard <zemzard@gmail.com>
2023-07-25 11:00:55 +0200

    Basic post endpoints for catching data from client side

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const server = require("http").createServer(app);
+//const server = require("http").createServer(app);
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -16,5 +16,27 @@ app.get("/status", async (req, res) => {
     res.send("im online");
     console.log("[ENDPOINT] GET STATUS");
   });
+
+
+app.post('/user-login', (req, res) => {
+
+    const requestData = req.body; //receive the request
+    console.log(req.body);
+    
+    //TODO ENSURE USER EXISTS, IF EXISTS LOG HIM IN IF NOT DENY
+
+    res.json({ message: 'POST request successfully received' });
+  });
+
+
+app.post('/user-register', (req, res) => {
+    
+    const requestData = req.body; //receive the register request
+    
+    //TODO ensure all fields are as expected, if they are - create new user
+
+    res.json({ message: 'POST request successfully received' });
+  });
+
 
 
